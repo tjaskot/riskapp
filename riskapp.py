6572bcf9e3301e6c9c9f9e34b040d6580d643445
trevor <trevor186@msn.com>
2019-02-04 21:25:15 -0500

    initial working directory structure python flask app riskapp

---
+++ riskapp.py
@@ -0,0 +1,58 @@
+### Imported packages ###
+# Flask
+# render_template
+# request
+# url_for
+#########################
+
+from flask import Flask, render_template, request, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello this is a application to determine risk for schedule and cost projections"
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = "Invalide username/password"
+            return redirect(url_for('loginerror', error=error))
+    return render_template('login.html')
+
+@app.route('/loginerror')
+#Syntax of python flask does not require variable from redirect to be passed into the below def function
+def loginerror():
+    #Checks to see if error is null, and catches exception
+    try:
+        #This code may raise an error if nothing is not null
+        error = error
+    except:
+        error = "This is my error."
+        app.logger.error("Application is passing null into loginerror function.")
+    return render_template('loginerror.html', error = error)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('notfound.html'), 404
+
+#Unit tests for application below (unit test code coverage +70%)
+#Validate that all url's are responsive
+with app.test_request_context('/hello', method='GET'):
+    #print(url_for(''))
+    assert request.path == '/hello'
+    assert request.method == 'GET'
+
+with app.test_request_context('/login', method='POST'):
+    assert request.path == '/login'
+    assert request.method == 'POST'
+
+with app.test_request_context('/loginerror'):
+    assert request.path == '/loginerror'
