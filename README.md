c7cb525e9268587e45bcb6e66117abf7523ce983
trevor <trevor186@msn.com>
2019-02-09 22:25:07 -0500

    initial overhaul of application to fit customer reflection.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 [![Build Status](https://travis-ci.org/tjaskot/riskapp.svg?branch=master)](https://travis-ci.org/tjaskot/riskapp)
 
 The .travis.yml file contains a script portion of the automated build. This script portion defaults to bash without any kernel language env being set.
+
+#User Defined Items
+The user defined variables are located in riskapp.py. These variables are then instantiated into the html templates on an as needed basis.
+
+#Html Reference
+The index.html is the home page for the application and loads with window.onload browser function.
+
+#Style Guide
+The common css sits under the static/common.css file. All other style sheets have replicated naming convention for html and css. Example: about.html located under templates/about.html wwould have an associating about.css file located under static/about.css.
+
+#Functionality Testing
+There are two ways to test this application locally prior to pushing the changes to Travis CI. The Foreman server can be leveraged to host local server and test changes all on port 5000, or you can leverage Flask's internal hosting.
+*Note: Travis CI will also test the build prior to deploying application to Heroku cloud in both the 2.7 and 3.5 versions of python.
--- riskapp.py
+++ riskapp.py
@@ -3,34 +3,47 @@
 # render_template
 # request
 # url_for
+# os
+# sys
 #########################
 
 from flask import Flask, render_template, request, url_for
 import os, sys
 app = Flask(__name__)
 
+# Define User Variables
+bkgrnd1 = "Department of Defense (DOD) software development issues drive cost overruns and delay in capability delivery Majority of DOD Major Defense Acquisition Programs (MDAPs) rely on software development to varying degrees ODASA-CE does not have reliable systems / tools available to conduct entity driven tradeoff analysis or risk assessment of software development projects"
+bkgrnd2d0 = "- No solution mimicking the DOD Architecture Framework (DODAF) is readily available"
+bkgrnd2d1 = "- No software or simulation based solution is readily available Objective: Develop, document, and deliver baseline model of Department of the Army (DA) software development teams in order to support"
+bkgrnd3 = "ODASA-CE analysis of projects including validation and verification (V/V) of program manager projections, staffing policy development, and interdependent processes exploration"
+poc1 = "Derek Eichin - Operations Research Analyst (ODASA)"
+poc2 = "Leo Pacatan - Systems 699 Student"
+poc3 = "Eric Jones - Systems 699 Student"
+poc4 = "Trevor Jaskot - Systems 699 Student"
+
+# Application Generated Routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', bkgrnd1=bkgrnd1, bkgrnd2d0=bkgrnd2d0, bkgrnd2d1=bkgrnd2d1, bkgrnd3=bkgrnd3)
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return "Hello this is a application to determine risk for schedule and cost projections"
+    return "Hello this will provide a demonstration of the application. It will show you the projections for a sample set of data and provide you with instructions on how to generate your own relative data sets."
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+@app.route('/generate', methods=['POST', 'GET'])
+def generate():
     error = None
     if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
             return log_the_user_in(request.form['username'])
         else:
             error = "Invalide username/password"
-            return redirect(url_for('loginerror', error=error))
-    return render_template('login.html')
+            return redirect(url_for('notfound.html', error=error))
+    return render_template('generate.html')
 
-@app.route('/loginerror')
+@app.route('/contacts')
 #Syntax of python flask does not require variable from redirect to be passed into the below def function
-def loginerror():
+def contacts():
     #Checks to see if error is null, and catches exception
     try:
         #This code may raise an error if nothing is not null
@@ -38,26 +51,27 @@ def loginerror():
     except:
         error = "This is my error."
         app.logger.error("Application is passing null into loginerror function.")
-    return render_template('loginerror.html', error = error)
+    return render_template('contacts.html', error = error)
 
 @app.errorhandler(404)
 def not_found(error):
     return render_template('notfound.html'), 404
 
 #Unit tests for application below (unit test code coverage +70%)
-#Validate that all url's are responsive
+#Validate that all url's are responsive and identify as themselves
 with app.test_request_context('/hello', method='GET'):
     #print(url_for(''))
     assert request.path == '/hello'
     assert request.method == 'GET'
 
-with app.test_request_context('/login', method='POST'):
-    assert request.path == '/login'
+with app.test_request_context('/generate', method='POST'):
+    assert request.path == '/generate'
     assert request.method == 'POST'
 
-with app.test_request_context('/loginerror'):
-    assert request.path == '/loginerror'
+with app.test_request_context('/contacts'):
+    assert request.path == '/contacts'
 
+# If environment is needed by host, then define variables below
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
