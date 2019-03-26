3a1c78bbbe1619d74b234ee615e1bccc0d195678
trevor <trevor186@msn.com>
2019-03-25 23:37:59 -0400

    adding correct images, finalizing base set of visuals with multiple inputs, adding script to set flask env vars

---
+++ setflaskvars.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+read -p "What is your app name? (Ex: app.py) " appname
+echo "Exporting Flask App as ${appname}."
+export FLASK_APP=${appname}
+
+read -p "Would you like to enable debugging mode? Please enter either 1 or 0. " bugmode
+if [[ ${bugmode} == 1 ]]; then
+	echo "I'm ENALBING Debug mode for flask."
+	export FLASK_DEBUG=1
+else
+	echo "I'm leaving debug mode OFF for now."	
+	export FLASK_DEBUG=0
+fi
+
+echo "I'm assuming you would like to use the default port of 5000, yes? "
+select defport in "yes" "other"; do
+	case ${defport} in
+		yes ) echo "I'm using the default port of 5000.";
+	                export FLASK_RUN_PORT=5000
+			break;;
+		other ) echo "I'm using your port value of ${defport}.";
+	                export FLASK_RUN_PORT=${defport}
+			break;;
+	esac
+done
+
+echo "What env? "
+select devprod in "Dev" "Prod"; do
+	case $devprod in
+		Dev ) export FLASK_ENV=development; break;;
+		Prod ) export FLASK_ENV=production; break;;
+	esac
+done
+
+while true; do
+	read -p "Do you want to start either a flask or foreman server (Yy/Nn)? " ff
+	case $ff in
+		[Yy]* ) echo "Which one? (Select 1 or 2) "
+			select forf in "foreman" "flask"; do
+				case $forf in
+					foreman ) echo "I'm starting a foreman server for you. Please make sure you have the package installed, otherwise this will fail."
+						foreman start;
+						break;;
+					flask ) echo "I have set your env vars, starting flask."
+						flask run;
+						break;;
+				esac
+			done;break;;
+		[Nn]* ) echo "Okay, please start the server before going to the localhost endpoint in your browser."; break;;
+		* ) echo "Please select one of the options."
+	esac
+done
--- static/js/generate.js
+++ static/js/generate.js
@@ -1,8 +1,16 @@
-// Enter a speed between 0 and 180
-var level = 125;
+// ----------------------------------------------------------------------
+// Schedule Variable Inputs
+schedVar1 = 20;
+schedVar2 = 20;
+schedVar3 = 20;
+schedVar4 = 20;
+schedVar5 = 20;
+
+// Total variable in schedule
+var schedLevel = schedVar1 + schedVar2 + schedVar3 + schedVar4 + schedVar5;
 
 // Trig to calc meter point
-var degrees = 180 - level,
+var degrees = 180 - schedLevel,
      radius = .5;
 var radians = degrees * Math.PI / 180;
 var x = radius * Math.cos(radians);
@@ -17,12 +25,12 @@ var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
      pathEnd = ' Z';
 var path = mainPath.concat(pathX,space,pathY,pathEnd);
 
-var data = [{ type: 'scatter',
+var schedData = [{ type: 'scatter',
    x: [0], y:[0],
-    marker: {size: 28, color:'rgba(0, 100, 200, 1)'},
+    marker: {size: 20, color:'rgba(0, 100, 200, 1)'},
     showlegend: true,
-    name: 'Schedule Integer (Interactive values below)',
-    text: level,
+    name: 'Schedule Value',
+    text: schedLevel,
     hoverinfo: 'text+name'},
   //Change these value to adjust chart circumference.
   { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
@@ -42,7 +50,7 @@ var data = [{ type: 'scatter',
   showlegend: true
 }];
 
-var layout = {
+var schedLayout = {
   shapes:[{
       type: 'path',
       path: path,
@@ -52,12 +60,156 @@ var layout = {
       }
     }],
   title: '<b>Schedule</b> <br> Scale 0-5',
-  height: 600,
-  width: 900,
+  // Height & Width replaced here with css values (userPlotlyChart) for reactive experience
   xaxis: {zeroline:false, showticklabels:false,
              showgrid: false, range: [-1, 1]},
   yaxis: {zeroline:false, showticklabels:false,
              showgrid: false, range: [-1, 1]}
 };
 
-Plotly.newPlot('schedulePlotChart', data, layout, {showSendToCloud:true});
+// ----------------------------------------------------------------------
+// Risk Variable Inputs
+riskVar1 = 25;
+riskVar2 = 25;
+riskVar3 = 25;
+riskVar4 = 25;
+riskVar5 = 25;
+
+// Total variable in schedule
+var riskLevel = riskVar1 + riskVar2 + riskVar3 + riskVar4 + riskVar5;
+
+// Trig to calc meter point
+var degrees = 180 - riskLevel,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+// Center pointer marker
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var riskData = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 20, color:'rgba(0, 100, 200, 1)'},
+    showlegend: true,
+    name: 'Risk Value',
+    text: riskLevel,
+    hoverinfo: 'text+name'},
+  //Change these value to adjust chart circumference.
+  { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+  rotation: 90,
+  text: ['Great', 'Very Good', 'Good', 'Fair',
+            'Poor', 'Very Poor', ''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(0, 210, 0, .8)', 'rgba(0, 200, 0, .5)',
+                         'rgba(0, 195, 0, .2)', 'rgba(195, 0, 0, .25)',
+                         'rgba(200, 0, 0, .5)', 'rgba(210, 0, 0, .8)',
+                         'rgba(255, 255, 255, 0)']},
+  labels: ['WayAhead', 'Ahead', 'OnTime', 'JustBehind', 'Behind', 'FarBehind', 'Full-Circle'],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: true
+}];
+
+var riskLayout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: 'rgba(0, 100, 200, 0.5)',
+      line: {
+        color: 'rgba(0, 100, 200, 0.5)'
+      }
+    }],
+  title: '<b>Risk</b> <br> Scale 0-5',
+  // Height & Width replaced here with css values (userPlotlyChart) for reactive experience
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+// ----------------------------------------------------------------------
+
+// ----------------------------------------------------------------------
+// Cost Variable Inputs
+costVar1 = 30;
+costVar2 = 30;
+costVar3 = 30;
+costVar4 = 30;
+costVar5 = 30;
+
+// Total variable in schedule
+var costLevel = costVar1 + costVar2 + costVar3 + costVar4 + costVar5;
+
+// Trig to calc meter point
+var degrees = 180 - costLevel,
+     radius = .5;
+var radians = degrees * Math.PI / 180;
+var x = radius * Math.cos(radians);
+var y = radius * Math.sin(radians);
+
+// Path: may have to change to create a better triangle
+// Center pointer marker
+var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+     pathX = String(x),
+     space = ' ',
+     pathY = String(y),
+     pathEnd = ' Z';
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var costData = [{ type: 'scatter',
+   x: [0], y:[0],
+    marker: {size: 20, color:'rgba(0, 100, 200, 1)'},
+    showlegend: true,
+    name: 'Cost Value',
+    text: costLevel,
+    hoverinfo: 'text+name'},
+  //Change these value to adjust chart circumference.
+  { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+  rotation: 90,
+  text: ['Great', 'Very Good', 'Good', 'Fair',
+            'Poor', 'Very Poor', ''],
+  textinfo: 'text',
+  textposition:'inside',
+  marker: {colors:['rgba(0, 210, 0, .8)', 'rgba(0, 200, 0, .5)',
+                         'rgba(0, 195, 0, .2)', 'rgba(195, 0, 0, .25)',
+                         'rgba(200, 0, 0, .5)', 'rgba(210, 0, 0, .8)',
+                         'rgba(255, 255, 255, 0)']},
+  labels: ['WayAhead', 'Ahead', 'OnTime', 'JustBehind', 'Behind', 'FarBehind', 'Full-Circle'],
+  hoverinfo: 'label',
+  hole: .5,
+  type: 'pie',
+  showlegend: true
+}];
+
+var costLayout = {
+  shapes:[{
+      type: 'path',
+      path: path,
+      fillcolor: 'rgba(0, 100, 200, 0.5)',
+      line: {
+        color: 'rgba(0, 100, 200, 0.5)'
+      }
+    }],
+  title: '<b>Cost</b> <br> Scale 0-5',
+  // Height & Width replaced here with css values (userPlotlyChart) for reactive experience
+  xaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]},
+  yaxis: {zeroline:false, showticklabels:false,
+             showgrid: false, range: [-1, 1]}
+};
+// ----------------------------------------------------------------------
+
+// Schedule Plot
+Plotly.newPlot('schedulePlotChart', schedData, schedLayout, {showSendToCloud:true});
+// Risk Plot
+Plotly.newPlot('riskPlotChart', riskData, riskLayout, {showSendToCloud:true});
+// Cost Plot
+Plotly.newPlot('costPlotChart', costData, costLayout, {showSendToCloud:true});
